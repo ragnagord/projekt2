1d702269eef54142be6b47fc617c8602185fea01
ragnagord <piter.cupakabra@gmail.com>
2015-02-23 19:31:54 +0000

    editing bookmarks

--- js/app.js
+++ js/app.js
@@ -46,6 +46,12 @@ angular.module('myApp',["ui.router", "myApp.factoryBookmarksService","myApp.fact
             $scope.startCreating();
         }
         
+        function updateBookmark(bookmark) {
+            BookmarksService.updateBookmark(bookmark);
+            $scope.editedBookmark = null;
+            $scope.isEditing = false;
+            $scope.isCreating = true;
+        }
         
         $scope.currentCategory = null;
         $scope.currentBookmark = null;
@@ -56,8 +62,86 @@ angular.module('myApp',["ui.router", "myApp.factoryBookmarksService","myApp.fact
         $scope.isCreatingCategory = true;
         $scope.isEditingCategory = false;
         
-         function shouldShowBookmark(){
+        function startCreating() {
+            $scope.isCreating = true;
+            $scope.isEditing = false;
+        }
+        
+        function startEditing() {
+            $scope.isCreating = false;
+            $scope.isEditing = true;
+        }
+        
+        function startCreatingCategory() {
+            $scope.isCreatingCategory = true;
+            $scope.isEditingCategory = false;
+        }
+        
+        function startEditingCategory() {
+            $scope.isCreatingCategory = false;
+            $scope.isEditingCategory = true;
+        }
+        
+        function shouldShowEditing() {
+            return $scope.isEditing;
+        }
+        
+        function shouldShowCreating() {
+            return $scope.isCreating;
+        }
+        
+        function shouldShowEditingCategory() {
+            return $scope.isEditingCategory;
+        }
+        
+        function shouldShowCreatingCategory() {
+            return $scope.isCreatingCategory;
+        }
+        
+        function setCurrentCategory(category) {
+            $scope.currentCategory = category;
+            $scope.currentBookmark = null;
+        }
+        
+        function isCurrentCategory(category) {
+            return $scope.currentCategory !== null && category.name === $scope.currentCategory.name;
+        }
+        
+        function isCurrentBookmark(bookmark) {
+            return $scope.currentBookmark !== null && bookmark.id === $scope.currentBookmark.id;
+        }
+        
+        function setCurrentBookmark(bookmark) {
+            $scope.currentBookmark = bookmark;
+        }
+        
+        function setEditedBookmark() {
+            $scope.editedBookmark = angular.copy($scope.currentBookmark);
+        }
+        
+        function setEditedCategory() {
+            $scope.editedCategory = angular.copy($scope.currentCategory);
+        }
+        
+        function shouldShowBookmark(){
             return $scope.currentBookmark !== null;
         }
+        
+        $scope.setCurrentCategory = setCurrentCategory;
+        $scope.setCurrentBookmark = setCurrentBookmark;
+        $scope.setEditedBookmark = setEditedBookmark;
+        $scope.startCreating = startCreating;
+        $scope.startEditing = startEditing;
+        $scope.shouldShowCreating = shouldShowCreating;
+        $scope.shouldShowEditing = shouldShowEditing;
+        $scope.updateBookmark = updateBookmark;
+        $scope.isCurrentCategory = isCurrentCategory;
+        $scope.isCurrentBookmark = isCurrentBookmark;
         $scope.shouldShowBookmark = shouldShowBookmark;
+        $scope.setEditedCategory = setEditedCategory;
+        $scope.startCreatingCategory = startCreatingCategory;
+        $scope.startEditingCategory = startEditingCategory;
+        $scope.shouldShowCreatingCategory = shouldShowCreatingCategory;
+        $scope.shouldShowEditingCategory = shouldShowEditingCategory;
+        $scope.updateCategory = updateCategory;
 })
